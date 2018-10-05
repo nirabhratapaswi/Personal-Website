7e3aeb17dc75297650653ca3a5b43554e60c05d6
Nirabhra Tapaswi <nirabhratapaswi@gmail.com>
2018-10-05 20:44:35 +0530

    Added svg for sidebar navigation, enlarged nagivation up-down buttons for mobiles.

--- assets/js/orange_new.js
+++ assets/js/orange_new.js
@@ -1,5 +1,6 @@
 var previous_top = 0,
     div_id_list = ["aboutMe", "portfolio", "projects", "workedAt", "skills"],
+    pages_loaded = [false, false, false, false, false],
     current_div = div_id_list[0],
     div_range = new Object(),
     sidebar_active_href = null;
@@ -44,13 +45,27 @@ var progress_bar_list = [{
     percentage: 75
 }];
 
-var showPage = () => {
+var showPage = () => { // NOTE: This takes the active href for current page calculation, so be sure to udpate active href before calling this!!
     $("#content > div").each(index => {
         // console.log("Searching:", sidebar_active_href.attr("href"), " :: ", "#".concat($($("#content > div")[index]).attr("id")));
         if (sidebar_active_href.attr("href") == "#".concat($($("#content > div")[index]).attr("id"))) {
-            // console.log("Flag 1 for: ", "#".concat($($("#content > div")[index]).attr("id")));
-            console.log("Index: ", index);
             if (index > 0) {
+                $(".header_class > ul li img").each(function(index_1) {
+                    console.log("Index----: ", index_1);
+                    if ($(this).siblings("a").attr("href") == sidebar_active_href.attr("href")) {
+                        if (index_1 % 2 == 0) {
+                            $(this).removeClass("icon_inactive");
+                        } else {
+                            $(this).addClass("icon_inactive");
+                        }
+                    } else {
+                        if (index_1 % 2 == 0) {
+                            $(this).addClass("icon_inactive");
+                        } else {
+                            $(this).removeClass("icon_inactive");
+                        }
+                    }
+                });
                 $("#content > div:nth-child(".concat((index + 1).toString(), ") .sectionTitle")).show("slide", {
                     direction: "up"
                 }, 900, () => {});
@@ -70,20 +85,15 @@ var showPage = () => {
                             $("#content > div:nth-child(".concat((index + 1).toString(), ") .sectionFooter")).show("slide", {
                                 direction: "down"
                             }, 1000, () => {});
-                            /*$('.skillbar').each(index_1 => {
-                                console.log("Skillbar children: ", index_1);
-                                $('.skillbar:nth-child('.concat((index_1 + 1).toString(), ")")).find('.skillbar-bar').animate({
-                                    width: $('.skillbar:nth-child('.concat((index_1 + 1).toString(), ")")).attr('data-percent')
-                                }, 1500);
-                            });*/
                             $('.skillbar').each(function() {
                                 $(this).find('.skillbar-bar').animate({
                                     width: $(this).attr('data-percent')
                                 }, 1500);
-                                setTimeout(() => {
-                                    $(".skillbar-bar").html("<div class='skillbar-circle'></div>");
-                                }, 1500);
                             });
+                            setTimeout(() => {
+                                $(".skillbar-bar").html("<div class='skillbar-square' style='display: none;'></div><div class='skillbar-square-new' style='display: none;'></div>");
+                                $(".skillbar-square, .skillbar-square-new").show("fade", {}, 1000, () => {});
+                            }, 1500);
                         }, 600, index);
                     }
                 }, 1000, index);
@@ -129,34 +139,62 @@ var gotoDiv = (div_index) => {
     });
 }
 
-var addScrollEventListenerFunction = (scroll_top) => {
+var showPageIfNotShown = page_index => {
+    if (!pages_loaded[page_index]) {
+        pages_loaded[page_index] = true;
+        showPage();
+        // return false;
+    }
+    // return true;
+}
+
+var changeIconForNav = () => { // Changes nav svg icon color to active
+    console.log("changeIconForNav called.");
+    $(".header_class > ul li img").each(function(index_1) {
+        console.log("Index----: ", index_1);
+        console.log($(this).siblings("a").attr("href"), " : ", sidebar_active_href.attr("href"));
+        if ($(this).siblings("a").attr("href") == sidebar_active_href.attr("href")) {
+            if (index_1 % 2 == 0) {
+                $(this).removeClass("icon_inactive");
+            } else {
+                $(this).addClass("icon_inactive");
+            }
+        } else {
+            if (index_1 % 2 == 0) {
+                $(this).addClass("icon_inactive");
+            } else {
+                $(this).removeClass("icon_inactive");
+            }
+        }
+    });
+}
+
+var scrollEventListener = (scroll_top) => {
+    let scroll_delay_subtraction = 0;
     scroll_top = $(this).scrollTop();
     if (scroll_top > previous_top) {
         // console.log("Scrolling down");
         current_div = whichDiv(scroll_top);
         if (checkIfDivEnd(current_div, scroll_top, true)) { // changing divs!!
             if (div_id_list.indexOf(current_div) < div_id_list.length - 1) {
-                console.log("Scrolling down, going to next div. Current div: ", current_div, ", target: ", div_range[div_id_list[div_id_list.indexOf(current_div) + 1]][0]);
+                // console.log("Scrolling down, going to next div. Current div: ", current_div, ", target: ", div_range[div_id_list[div_id_list.indexOf(current_div) + 1]][0]);
                 gotoDiv(div_id_list[div_id_list.indexOf(current_div) + 1]);
-                $(window).off("scroll", addScrollEventListenerFunction);
+                $(window).off("scroll", scrollEventListener);
                 setTimeout(() => {
-                    $(window).on("scroll", addScrollEventListenerFunction);
+                    $(window).on("scroll", scrollEventListener);
                 }, delay_time_for_event_listener);
             }
-        } else {
-            console.log("Scrolling down, not changing div: ", current_div);
         }
+        scroll_delay_subtraction = delay_time_for_event_listener;
     } else if (scroll_top < previous_top) {
         // console.log("Scrolling up")
         if (checkIfDivEnd(current_div, scroll_top, false)) { // changing divs!!
             gotoDiv(div_id_list[div_id_list.indexOf(current_div) - 1]);
-            $(window).off("scroll", addScrollEventListenerFunction);
+            $(window).off("scroll", scrollEventListener);
             setTimeout(() => {
-                $(window).on("scroll", addScrollEventListenerFunction);
+                $(window).on("scroll", scrollEventListener);
             }, delay_time_for_event_listener);
-            console.log("Scrolling up, going to previous div Current div: ", current_div);
-        } else {
-            console.log("Scrolling up, not changing div: ", current_div);
+            // console.log("Scrolling up, going to previous div Current div: ", current_div);
         }
         current_div = whichDiv(scroll_top);
     }
@@ -165,14 +203,16 @@ var addScrollEventListenerFunction = (scroll_top) => {
     });
     $(".header_class > ul li").each(function(index) {
         if ($(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).attr("href") == "#".concat(current_div)) {
-            console.log();
             $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).addClass("active");
             sidebar_active_href = $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a"));
         } else {
             $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).removeClass("active");
         }
     });
-    showPage();
+    changeIconForNav();
+    setTimeout(() => {
+        showPageIfNotShown(div_id_list.indexOf(current_div));
+    }, delay_time_for_event_listener - scroll_delay_subtraction);
     previous_top = scroll_top;
 }
 
@@ -181,12 +221,6 @@ var summation = () => {
         arguments[i] += arguments[i - 1];
     }
     return arguments[arguments.length - 1];
-}
-
-var checkChildrenHeights = (jqueryElementId) => {
-    $("#".concat(jqueryElementId)).children("div").each(index => {
-        // console.log("Index: ", index, ", height: ", $("#".concat(jqueryElementId, " div:nth-child(", (index+1).toString(), ")")).height());
-    });
 }
 
 var resizeBasedOnWindowForScroll = () => {
@@ -201,7 +235,6 @@ var resizeBasedOnWindowForScroll = () => {
         let start_pos = 0,
             end_pos;
         for (let div in div_id_list) {
-            checkChildrenHeights(div_id_list[div]);
             if (div < div_id_list.length - 1) {
                 // console.log($("#".concat(div_id_list[div])), " : ", $("#".concat(div_id_list[parseInt(div)+1])));
                 div_range[div_id_list[div]] = [$("#".concat(div_id_list[div])).offset().top, $("#".concat(div_id_list[parseInt(div) + 1])).offset().top];
@@ -209,18 +242,77 @@ var resizeBasedOnWindowForScroll = () => {
                 // console.log($("#".concat(div_id_list[div])).offset().top, " : ", $("#".concat(div_id_list[div])).offset().top + $("#".concat(div_id_list[div])).height());
                 div_range[div_id_list[div]] = [$("#".concat(div_id_list[div])).offset().top, $("#".concat(div_id_list[div])).offset().top + $("#".concat(div_id_list[div])).height()];
             }
-            console.log("For index: ", div, ", ele: ", div_id_list[div], ", height: ", div_range[div_id_list[div]][1] - div_range[div_id_list[div]][0]);
+            // console.log("For index: ", div, ", ele: ", div_id_list[div], ", height: ", div_range[div_id_list[div]][1] - div_range[div_id_list[div]][0]);
         }
-        console.log(div_range);
         resized = false;
     } else {
         setTimeout(resizeBasedOnWindowForScroll, 50);
     }
 }
 
+var disableScrollClickAndEventListeners = () => {
+    $(window).off("scroll", scrollEventListener);
+    $(".scroll_down, .scroll_up").off("click");
+    setTimeout(() => {
+        $(window).on("scroll", scrollEventListener);
+        $(".scroll_down").on("click", scrollDownClickEvent);
+        $(".scroll_up").on("click", scrollUpClickEvent);
+    }, delay_time_for_event_listener + 100);
+}
+
+var scrollDownClickEvent = scroll_top => {
+    if (div_id_list.indexOf(current_div) < div_id_list.length - 1) {
+        gotoDiv(div_id_list[div_id_list.indexOf(current_div) + 1]);
+        disableScrollClickAndEventListeners(delay_time_for_event_listener);
+    }
+    setTimeout(() => { // to display the target page.
+        scroll_top = $(this).scrollTop();
+        current_div = whichDiv(scroll_top);
+        $("#".concat(current_div, " > .hiddenSectionTitle")).css({
+            "display": "none"
+        });
+        $(".header_class > ul li").each(function(index) {
+            if ($(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).attr("href") == "#".concat(current_div)) {
+                $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).addClass("active");
+                sidebar_active_href = $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a"));
+            } else {
+                $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).removeClass("active");
+            }
+        });
+        changeIconForNav();
+        showPageIfNotShown(div_id_list.indexOf(current_div));
+    }, delay_time_for_event_listener);
+}
+
+var scrollUpClickEvent = scroll_top => {
+    if (div_id_list.indexOf(current_div) > 0) {
+        gotoDiv(div_id_list[div_id_list.indexOf(current_div) - 1]);
+        disableScrollClickAndEventListeners(delay_time_for_event_listener);
+    }
+    setTimeout(() => { // to display the target page.
+        scroll_top = $(this).scrollTop();
+        current_div = whichDiv(scroll_top);
+        $("#".concat(current_div, " > .hiddenSectionTitle")).css({
+            "display": "none"
+        });
+        $(".header_class > ul li").each(function(index) {
+            if ($(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).attr("href") == "#".concat(current_div)) {
+                $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).addClass("active");
+                sidebar_active_href = $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a"));
+            } else {
+                $(".header_class > ul li:nth-child(".concat((index + 1).toString(), ") a")).removeClass("active");
+            }
+        });
+        changeIconForNav();
+        showPageIfNotShown(div_id_list.indexOf(current_div));
+    }, delay_time_for_event_listener);
+}
+
 $(() => {
     resizeBasedOnWindowForScroll();
-    $(window).on("scroll", addScrollEventListenerFunction);
+
+    // Add Scroll EventListener
+    $(window).on("scroll", scrollEventListener);
 
     //  For jquery animations
 
@@ -228,48 +320,28 @@ $(() => {
     $("#".concat(current_div, " > .hiddenSectionTitle")).css({
         "display": "none"
     });
-    showPage();
-    $(".header_class a").click(function(e) {
-        // e.preventDefault();  // uncomment this and fix move!
-        $(this).parent().siblings("li").children("a").removeClass("active");
-        $(this).addClass("active");
-        sidebar_active_href = $(this);
-        showPage();
-    });
+    showPageIfNotShown(div_id_list.indexOf(current_div));
 
     $(window).resize(resizeBasedOnWindowForScroll);
 
-    $(".header_class > ul li a").click(function() {
-        console.log("header class element clicked: ", $(this).attr("href"));
-        $(window).off("scroll", addScrollEventListenerFunction);
-        setTimeout(() => {
-            $(window).on("scroll", addScrollEventListenerFunction);
-        }, delay_time_for_event_listener + 100);
-        console.log();
+    $(".header_class > ul li a").click(function() { // click on sidebar links!
+        $(this).parent().siblings("li").children("a").removeClass("active");
+        $(this).addClass("active");
+        sidebar_active_href = $(this);
+        disableScrollClickAndEventListeners(delay_time_for_event_listener);
+        setTimeout(() => { // to display the target page.
+            current_div = whichDiv($(this).scrollTop());
+            showPageIfNotShown(div_id_list.indexOf($(this).attr("href").split("#")[1]));
+        }, delay_time_for_event_listener);
         $($(this).attr("href").concat(" > .hiddenSectionTitle")).css({
             "display": "none"
         });
+        changeIconForNav();
         gotoDiv($(this).attr("href").split("#")[1]);
     });
 
-    $(".scroll_down").click(function() {
-        if (div_id_list.indexOf(current_div) < div_id_list.length - 1) {
-            console.log("Scrolling down, going to next div. Current div: ", current_div, ", target: ", div_range[div_id_list[div_id_list.indexOf(current_div) + 1]][0]);
-            gotoDiv(div_id_list[div_id_list.indexOf(current_div) + 1]);
-            $(window).off("scroll", addScrollEventListenerFunction);
-            setTimeout(() => {
-                $(window).on("scroll", addScrollEventListenerFunction);
-            }, delay_time_for_event_listener);
-        }
-    });
-    $(".scroll_up").click(function() {
-        if (div_id_list.indexOf(current_div) > 0) {
-            gotoDiv(div_id_list[div_id_list.indexOf(current_div) - 1]);
-            $(window).off("scroll", addScrollEventListenerFunction);
-            setTimeout(() => {
-                $(window).on("scroll", addScrollEventListenerFunction);
-            }, delay_time_for_event_listener);
-            console.log("Scrolling up, going to previous div Current div: ", current_div);
-        }
-    });
+    // Click functions for the up and down navigations buttons! -------------------------------------------------------------------------
+    $(".scroll_down").click(scrollDownClickEvent);
+    $(".scroll_up").click(scrollUpClickEvent);
+    // END------------------------------------------------------------------------------------------------------------------------------
 });
